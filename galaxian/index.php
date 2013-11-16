5fb5639150062219fb0662a81622d32c86ecf170
Anders Akesson <andy_akesson@me.com>
2013-11-16 18:09:37 +0100

    Part 2
    Added more layers and a ship.

--- galaxian/index.php
+++ galaxian/index.php
@@ -1,10 +1,17 @@
 <?php $title='Anders Akesson | Galaxian'; include(__DIR__ . '/../mall/header.php'); ?>
 <body onload="init()">
 	<div id='flash'>
-	<h1>Galaxian</h1>
-	<canvas id='background' width='600' height='360'>
-	  Your browser does not support the element HTML5 Canvas.
-	</canvas>
-	</div>
-	<script src="space_shooter_part_one.js"></script>
+		<h1>Galaxian</h1>
+		<canvas id='background' width='800' height='480'>
+		  Your browser does not support the element HTML5 Canvas.
+		</canvas>
+		    <!-- The canvas for all enemy ships and bullets -->
+	    <canvas id="main" width="800" height="480">
+	    </canvas>
+	    <!-- The canvas the ship uses (can only move up
+	         one forth the screen.) -->
+	    <canvas id="ship" width="800" height="480">
+	    </canvas>
+	</div><!--Flash ends-->
+	<script src="space_shooter.js"></script>
   <?php $path=__DIR__; include(__DIR__ . '/../mall/footer.php'); ?>
